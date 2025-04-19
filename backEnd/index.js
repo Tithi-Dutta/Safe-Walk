95d2938a61ef94b87fe6d67ee81ddd6cf0c631aa
anubratanaskar <anubrata.naskar@gmail.com>
2025-04-19 13:54:25 +0530

    access url change http -> https, in accessServ.js has little issue

--- backEnd/index.js
+++ backEnd/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const path = require('path');
 const cors = require('cors');
-
+const accessServ = require('./accessServ');
 const bodyParser = require('body-parser');
 const app = express();
 
@@ -14,8 +14,24 @@ app.use(express.static(path.join(__dirname, '../frontEnd')));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const PORT = 5000;
+
+app.use('/location', accessServ);
+
+/*const PORT = 5000;
 const HOST = 'localhost'; 
 app.listen(PORT, HOST, () => {
   console.log(`Server started at http://${HOST}:${PORT}`);
+});*/
+const fs = require('fs');
+const https = require('https');
+const PORT = 5000;
+const HOST = '0.0.0.0';
+
+const options = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem')
+};
+
+https.createServer(options, app).listen(PORT, HOST, () => {
+  console.log(`Secure server started at https://${HOST}:${PORT}`);
 });
